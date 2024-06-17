f1a5882de50ed76135b5faf372d404c938928d60
Kaio-code-creator <kaioasousa12@gmail.com>
2024-06-17 16:23:49 -0300

    Initial commit

---
+++ Matematica/logica-basica-001/questions.js
@@ -0,0 +1,103 @@
+let questions = [
+    {
+        question: 'Qual os próximos números 1,3,5,7,...?',
+        options: [
+            '9,10,11',
+            '9,11,13',
+            '10.12.14',
+            '9.14.18'
+        ],
+        answer: 1
+    },
+    {
+        question: 'Qual os próximos números 0,2,4,6,...?',
+        options: [
+            '10,14,16',
+            '9,10,12',
+            '8,10,12'
+        ],
+        answer: 2
+    },
+    {
+        question: 'Qual os próximos números 0,5,10,15,...?',
+        options: [
+            '16,26,36',
+            '20,25,30',
+            '22,26,30'
+        ],
+        answer: 1
+    },
+    {
+        question: 'Qual os próximos números 1,2,3,5,8,13,...',
+        options: [
+            '14,15,16',
+            '21,34,55',
+            '10,20,30',
+            '20,28,36'
+        ],
+        answer: 1
+    },
+    {
+        question: 'Qual os próximos números 10,20,30,...?',
+        options: [
+            '50,60,70',
+            '40,45,60',
+            '40,60,80',
+            '40,50,60'
+        ],
+        answer: 3
+    },
+    {
+        question: 'Dessas palavras, qual a única que não pertence a mesma classe das outras?',
+        options: [
+            'vaca',
+            'cavalo',
+            'coelho',
+            'burro',
+            'jumento'
+        ],
+        answer: 2
+    },
+    {
+        question: 'Dessas palavras, qual a única que não pertence a mesma classe das outras?',
+        options: [
+            'rato',
+            'coelho',
+            'cachorro',
+            'preá',
+            'camundongo'
+        ],
+        answer: 2
+    },
+    {
+        question: 'Dessas palavras, qual a única que não pertence a mesma classe das outras?',
+        options: [
+            'baleia',
+            'golfinho',
+            'gato',
+            'tubarão'
+        ],
+        answer: 2
+    },
+    {
+        question: 'Dessas palavras, qual a única que não pertence a mesma classe das outras?',
+        options: [
+            'leão',
+            'galinha',
+            'pavão',
+            'peru (ave)'
+        ],
+        answer: 0
+    },
+    {
+        question: 'Dessas palavras, qual a única que não pertence a mesma classe das outras?',
+        options: [
+            'peixe',
+            'jacaré',
+            'sapo',
+            'foca',
+            'crocodilo'
+        ],
+        answer: 0
+    },
+];
