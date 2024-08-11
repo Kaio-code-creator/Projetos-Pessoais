43287c29228f475656b9d899bfde0c69928820d6
Kaio-code-creator <kaioasousa12@gmail.com>
2024-08-11 17:00:31 -0300

    Temas escuro

--- Comparador de Preços/script.js
+++ Comparador de Preços/script.js
@@ -56,3 +56,15 @@ function comparePrices() {
 
     document.getElementById('result').innerText = resultText;
 }
+
+function toggleTheme() {
+    const body = document.body;
+    body.classList.toggle('dark-theme');
+    
+    const themeButton = document.getElementById('themeToggle');
+    if (body.classList.contains('dark-theme')) {
+        themeButton.innerText = 'Tema Claro';
+    } else {
+        themeButton.innerText = 'Tema Escuro';
+    }
+}
